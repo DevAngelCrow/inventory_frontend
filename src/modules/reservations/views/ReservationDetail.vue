10eb22b9556d0e65063f5da35e3a408db40ed9ef
angel_dev <angelguille1@hotmail.com>
2026-07-18 12:24:29 -0600

    feat: implement reservation detail view and useReservation composable for managing rental forms and cart operations

--- src/modules/reservations/views/ReservationDetail.vue
+++ src/modules/reservations/views/ReservationDetail.vue
@@ -1,23 +1,12 @@
 <template>
   <div class="py-5 px-5 h-full max-h-full flex items-start justify-center">
-    <section
-      id="reservation_detail_content"
-      class="w-full xl:w-[95%] flex flex-col gap-6"
-    >
+    <section id="reservation_detail_content" class="w-full xl:w-[95%] flex flex-col gap-6">
       <div class="flex items-center gap-3">
-        <Button
-          icon="pi pi-arrow-left"
-          severity="secondary"
-          outlined
-          @click="goBack"
-        />
+        <Button icon="pi pi-arrow-left" severity="secondary" outlined @click="goBack" />
         <AppTitle :title="pageTitle" />
       </div>
 
-      <form
-        @submit.prevent="onSubMit"
-        class="grid grid-cols-1 lg:grid-cols-3 gap-6"
-      >
+      <form @submit.prevent="onSubMit" class="grid grid-cols-1 lg:grid-cols-3 gap-6">
         <!-- Event Info Card (Left Column) -->
         <AppCard class="lg:col-span-2">
           <template #title>
@@ -25,119 +14,50 @@
           </template>
           <template #content>
             <div class="grid grid-cols-1 md:grid-cols-2 gap-5">
-              <AppSelect
-                class="w-full"
-                id="id_customer"
-                label="Cliente*"
-                v-model="id_customer"
-                :error-messages="errors.id_customer"
-                v-bind="idCustomerAttrs"
-                :options="customersList"
-                optionLabel="first_name"
-                optionValue="id"
-                :readonly="isReadonly"
-              />
+              <AppSelect class="w-full" id="id_customer" label="Cliente*" v-model="id_customer"
+                :error-messages="errors.id_customer" v-bind="idCustomerAttrs" :options="customersList"
+                optionLabel="first_name" optionValue="id" :readonly="isReadonly" />
 
               <div class="flex justify-end items-center" v-if="status">
-                <AppChipStatus
-                  :label="computedStatusLabel"
-                  :backgroundColor="computedStatusColor"
-                />
+                <AppChipStatus :label="computedStatusLabel" :backgroundColor="computedStatusColor" />
               </div>
 
               <div
-                class="md:col-span-2 flex flex-col gap-4 mt-2 mb-2 p-4 border rounded-md bg-gray-50 dark:bg-gray-800"
-              >
-                <h4
-                  class="font-semibold text-lg text-surface-900 dark:text-surface-0"
-                >
+                class="md:col-span-2 flex flex-col gap-4 mt-2 mb-2 p-4 border rounded-md bg-gray-50 dark:bg-gray-800">
+                <h4 class="font-semibold text-lg text-surface-900 dark:text-surface-0">
                   Dirección de Entrega
                 </h4>
                 <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
-                  <AppSelect
-                    class="w-full md:col-span-2"
-                    id="id_customer_address"
-                    label="Rellenar con dirección guardada del cliente"
-                    v-model="id_customer_address"
-                    :error-messages="errors.id_customer_address"
-                    v-bind="idCustomerAddressAttrs"
-                    :options="customerAddresses"
-                    optionLabel="label"
-                    optionValue="id"
-                    :readonly="isReadonly"
-                    @change="onCustomerAddressChange"
-                  />
+                  <AppSelect class="w-full md:col-span-2" id="id_customer_address"
+                    label="Rellenar con dirección guardada del cliente" v-model="id_customer_address"
+                    :error-messages="errors.id_customer_address" v-bind="idCustomerAddressAttrs"
+                    :options="customerAddresses" optionLabel="label" optionValue="id" :readonly="isReadonly"
+                    @change="onCustomerAddressChange" />
 
-                  <AppInputText
-                    class="w-full"
-                    id="delivery_address"
-                    label="Dirección Línea 1"
-                    v-model="delivery_address"
-                    :error-messages="errors.delivery_address"
-                    v-bind="deliveryAddressAttrs"
-                    :readonly="isReadonly"
-                  />
-                  <AppInputText
-                    class="w-full"
-                    id="delivery_address_line2"
-                    label="Dirección Línea 2"
-                    v-model="delivery_address_line2"
-                    :error-messages="errors.delivery_address_line2"
-                    v-bind="deliveryAddressLine2Attrs"
-                    :readonly="isReadonly"
-                  />
+                  <AppInputText class="w-full" id="delivery_address" label="Dirección Línea 1"
+                    v-model="delivery_address" :error-messages="errors.delivery_address" v-bind="deliveryAddressAttrs"
+                    :readonly="isReadonly" />
+                  <AppInputText class="w-full" id="delivery_address_line2" label="Dirección Línea 2"
+                    v-model="delivery_address_line2" :error-messages="errors.delivery_address_line2"
+                    v-bind="deliveryAddressLine2Attrs" :readonly="isReadonly" />
 
-                  <AppGeographicCascade
-                    class="w-full"
-                    id="id_geographic_division"
-                    v-model="id_geographic_division"
-                    :error-messages="errors.id_geographic_division"
-                    :id_country="selectedCustomerCountryId"
-                    :readonly="isReadonly"
-                  />
-                  <AppInputText
-                    class="w-full"
-                    id="delivery_zip"
-                    label="Código Postal"
-                    v-model="delivery_zip"
-                    :error-messages="errors.delivery_zip"
-                    v-bind="deliveryZipAttrs"
-                    :readonly="isReadonly"
-                  />
+                  <AppGeographicCascade class="w-full" id="id_geographic_division" v-model="id_geographic_division"
+                    :error-messages="errors.id_geographic_division" :id_country="selectedCustomerCountryId"
+                    :readonly="isReadonly" />
+                  <AppInputText class="w-full" id="delivery_zip" label="Código Postal" v-model="delivery_zip"
+                    :error-messages="errors.delivery_zip" v-bind="deliveryZipAttrs" :readonly="isReadonly" />
 
-                  <AppInputextArea
-                    class="w-full md:col-span-2"
-                    id="delivery_notes"
-                    label="Notas de Entrega"
-                    v-model="delivery_notes"
-                    :error-messages="errors.delivery_notes"
-                    v-bind="deliveryNotesAttrs"
-                    :readonly="isReadonly"
-                  />
+                  <AppInputextArea class="w-full md:col-span-2" id="delivery_notes" label="Notas de Entrega"
+                    v-model="delivery_notes" :error-messages="errors.delivery_notes" v-bind="deliveryNotesAttrs"
+                    :readonly="isReadonly" />
                 </div>
               </div>
 
-              <AppDatePicker
-                class="w-full"
-                id="event_start"
-                label="Inicio del Evento*"
-                v-model="event_start"
-                :error-messages="errors.event_start"
-                v-bind="eventStartAttrs"
-                showTime
-                :readonly="isReadonly"
-              />
+              <AppDatePicker class="w-full" id="event_start" label="Inicio del Evento*" v-model="event_start"
+                :error-messages="errors.event_start" v-bind="eventStartAttrs" showTime :readonly="isReadonly" />
 
-              <AppDatePicker
-                class="w-full"
-                id="event_end"
-                label="Fin del Evento*"
-                v-model="event_end"
-                :error-messages="errors.event_end"
-                v-bind="eventEndAttrs"
-                showTime
-                :readonly="isReadonly"
-              />
+              <AppDatePicker class="w-full" id="event_end" label="Fin del Evento*" v-model="event_end"
+                :error-messages="errors.event_end" v-bind="eventEndAttrs" showTime :readonly="isReadonly" />
             </div>
           </template>
         </AppCard>
@@ -152,78 +72,42 @@
               <div class="flex flex-col gap-4">
                 <div class="flex justify-between border-b pb-2">
                   <span>Subtotal Mobiliario:</span>
-                  <span class="font-semibold"
-                    >${{ cartSubtotal.toFixed(2) }}</span
-                  >
+                  <span class="font-semibold">${{ cartSubtotal.toFixed(2) }}</span>
                 </div>
+                <!--
                 <div class="flex justify-between border-b pb-2">
                   <span>Impuestos (13% IVA):</span>
                   <span class="font-semibold"
                     >${{ cartTaxAmount.toFixed(2) }}</span
                   >
                 </div>
+                -->
 
-                <AppInputMoney
-                  class="w-full"
-                  id="delivery_fee"
-                  label="Costo de Envío"
-                  v-model="delivery_fee"
-                  :error-messages="errors.delivery_fee"
-                  v-bind="deliveryFeeAttrs"
-                  :readonly="isReadonly"
-                />
+                <AppInputMoney class="w-full" id="delivery_fee" label="Costo de Envío" v-model="delivery_fee"
+                  :error-messages="errors.delivery_fee" v-bind="deliveryFeeAttrs" :readonly="isReadonly" />
 
-                <AppInputMoney
-                  class="w-full"
-                  id="discount_amount"
-                  label="Descuento Aplicado"
-                  v-model="discount_amount"
-                  :error-messages="errors.discount_amount"
-                  v-bind="discountAmountAttrs"
-                  :readonly="isReadonly"
-                />
+                <AppInputMoney class="w-full" id="discount_amount" label="Descuento Aplicado" v-model="discount_amount"
+                  :error-messages="errors.discount_amount" v-bind="discountAmountAttrs" :readonly="isReadonly" />
 
-                <div
-                  class="flex justify-between border-b pb-2 text-lg font-bold"
-                >
+                <div class="flex justify-between border-b pb-2 text-lg font-bold">
                   <span>Total Alquiler:</span>
                   <span class="text-primary">${{ cartTotal.toFixed(2) }}</span>
                 </div>
 
-                <AppInputMoney
-                  class="w-full"
-                  id="deposit_amount"
-                  label="Anticipo / Depósito Pagado"
-                  v-model="deposit_amount"
-                  :error-messages="errors.deposit_amount"
-                  v-bind="depositAmountAttrs"
-                  :readonly="isReadonly"
-                />
+                <AppInputMoney class="w-full" id="deposit_amount" label="Anticipo / Depósito Pagado"
+                  v-model="deposit_amount" :error-messages="errors.deposit_amount" v-bind="depositAmountAttrs"
+                  :readonly="isReadonly" />
 
-                <div
-                  class="flex justify-between text-lg font-bold text-red-600"
-                >
+                <div class="flex justify-between text-lg font-bold text-red-600">
                   <span>Saldo Pendiente:</span>
                   <span>${{ cartBalanceDue.toFixed(2) }}</span>
                 </div>
 
-                <AppInputextArea
-                  class="w-full"
-                  id="notes"
-                  label="Notas Factura"
-                  v-model="notes"
-                  :error-messages="errors.notes"
-                  v-bind="notesAttrs"
-                  :readonly="isReadonly"
-                />
+                <AppInputextArea class="w-full" id="notes" label="Notas Factura" v-model="notes"
+                  :error-messages="errors.notes" v-bind="notesAttrs" :readonly="isReadonly" />
 
                 <div class="flex gap-2 justify-end mt-4" v-if="!isReadonly">
-                  <Button
-                    label="Cancelar"
-                    severity="secondary"
-                    outlined
-                    @click="goBack"
-                  />
+                  <Button label="Cancelar" severity="secondary" outlined @click="goBack" />
                   <Button label="Guardar Reserva" type="submit" />
                 </div>
               </div>
@@ -238,36 +122,16 @@
               <h3>Artículos Seleccionados para Alquiler</h3>
               <!-- Add Item Form (Inline) -->
               <div class="flex flex-wrap gap-3 items-center" v-if="!isReadonly">
-                <AppSelect
-                  class="flex-1 min-w-[300px]"
-                  id="add_product"
-                  label="Seleccionar Producto"
-                  v-model="selectedProductToAdd"
-                  :options="productsList"
-                  optionLabel="name"
-                />
-                <AppInputNumber
-                  class="flex grow"
-                  id="add_qty"
-                  label="Cant."
-                  v-model="selectedQtyToAdd"
-                />
-                <Button
-                  label="Agregar"
-                  class="flex grow rounded-md"
-                  icon="pi pi-plus"
-                  size="small"
-                  @click="addItemToCart"
-                />
+                <AppSelect class="flex-1 min-w-[300px]" id="add_product" label="Seleccionar Producto"
+                  v-model="selectedProductToAdd" :options="productsList" optionLabel="name" />
+                <AppInputNumber class="flex grow" id="add_qty" label="Cant." v-model="selectedQtyToAdd" />
+                <Button label="Agregar" class="flex grow rounded-md" icon="pi pi-plus" size="small"
+                  @click="addItemToCart" />
               </div>
             </div>
           </template>
           <template #content>
-            <AppDataTable
-              :items="cartItems"
-              :headers="cartHeaders"
-              class="p-datatable-sm w-full"
-            >
+            <AppDataTable :items="cartItems" :headers="cartHeaders" class="p-datatable-sm w-full">
               <template #body-unit_price="{ data }">
                 ${{ Number(data.unit_price).toFixed(2) }}
               </template>
@@ -275,19 +139,11 @@
                 ${{ Number(data.subtotal).toFixed(2) }}
               </template>
               <template #body-accion="{ data }">
-                <Button
-                  icon="pi pi-trash"
-                  severity="danger"
-                  variant="text"
-                  rounded
-                  @click="removeFromCart(data.id_product)"
-                />
+                <Button icon="pi pi-trash" severity="danger" variant="text" rounded
+                  @click="removeFromCart(data.id_product)" />
               </template>
             </AppDataTable>
-            <div
-              v-if="!cartItems.length"
-              class="text-center py-5 text-gray-500"
-            >
+            <div v-if="!cartItems.length" class="text-center py-5 text-gray-500">
               No hay productos agregados en el carrito para esta reserva.
             </div>
           </template>
@@ -364,12 +220,13 @@ const {
   setReservationItem,
   cartItems,
   cartSubtotal,
-  cartTaxAmount,
+  //cartTaxAmount,
   cartTotal,
   cartBalanceDue,
   customersList,
   productsList,
   handleSubmit,
+  currentReservation,
 } = reservationInstance;
 
 const selectedProductToAdd = ref<ProductResponse>();
@@ -387,10 +244,8 @@ const customerAddresses = computed(() => {
 });
 
 const computedStatusLabel = computed(() => {
-  if (!status.value) return '';
-  const baseStatus = status.value as
-    | { code: string; name: string; state_color: string }
-    | undefined;
+  if (!currentReservation.value?.status) return '';
+  const baseStatus = currentReservation.value.status;
   if (baseStatus?.code === 'IN_PROGRESS') {
     if (pickup_datetime.value) return 'Retornado al almacén';
     if (delivery_datetime.value) return 'Entregado al cliente';
@@ -399,10 +254,8 @@ const computedStatusLabel = computed(() => {
 });
 
 const computedStatusColor = computed(() => {
-  if (!status.value) return '';
-  const baseStatus = status.value as
-    | { code: string; name: string; state_color: string }
-    | undefined;
+  if (!currentReservation.value?.status) return '';
+  const baseStatus = currentReservation.value.status;
   if (baseStatus?.code === 'IN_PROGRESS') {
     if (pickup_datetime.value) return '#f59e0b'; // amber
     if (delivery_datetime.value) return '#10b981'; // emerald
